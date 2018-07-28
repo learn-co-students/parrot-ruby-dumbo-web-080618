ea5b01ea11454b46eb7fd4c00cbd0d6308daa42a
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-28 22:43:18 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot 
-  puts "Polly want a cracker"
-  "Polly want a cracker"
+def parrot optional = "Squawk!"
+  puts optional
+  optional
 end
